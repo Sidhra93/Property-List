031a3a3cdaf8e01a01573a879456a23e5aff4cb6
Sidhra <sidhra.cader@hotmail.com>
2017-12-06 19:17:05 +1100

    display data on page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import Data from './data'
 import Card from './components/Card'
 
 class App extends Component {
+  state = {
+    results: Data.results,
+    saved: Data.saved
+  }
   render() {
     return (
       <div className='container'>
         <div className='row'>
-          <div className='col'>
+          <div className='col-6 results'>
             <h3>Results</h3>
-            <Card />
-            <Card />
-            <Card />            
+            {this.state.results.map((property, index) =>
+              <Card
+                logo={property['agency']['logo']}
+                image={property['mainImage']}
+                price={property['price']}
+              />
+            )}
+
           </div>
-          <div className='col'>
+          <div className='col-6 saved'>
             <h3>Saved Properties</h3>
-            <Card />
+            {this.state.saved.map((property, index) =>
+              <Card
+                logo={property['agency']['logo']}
+                image={property['mainImage']}
+                price={property['price']}
+              />
+            )}
           </div>
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-export default function Card(){
+export default function Card(props){
   return(
     <div className='card'>
       <div>
-        <img src='http://via.placeholder.com/350x50' />
+        <img src={props.logo} alt=''/>
+      </div>
+      <div className='image'>
+        <img src={props.image} alt=''/>
       </div>
       <div>
-        <img src='http://via.placeholder.com/350x500' />
+        {props.price}
       </div>
-      <div>$576.00</div>
     </div>
   )
 }
