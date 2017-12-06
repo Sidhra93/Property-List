5e2da67260f93e2fd729502f9a3e50cac5c37fcf
Sidhra <sidhra.cader@hotmail.com>
2017-12-06 18:32:12 +1100

    create base design without data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Data from './data'
+import Card from './components/Card'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <h3>Results</h3>
+            <Card />
+            <Card />
+            <Card />            
+          </div>
+          <div className='col'>
+            <h3>Saved Properties</h3>
+            <Card />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Card(){
+  return(
+    <div className='card'>
+      <div>
+        <img src='http://via.placeholder.com/350x50' />
+      </div>
+      <div>
+        <img src='http://via.placeholder.com/350x500' />
+      </div>
+      <div>$576.00</div>
+    </div>
+  )
+}
